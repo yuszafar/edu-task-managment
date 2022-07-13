b3b46bdf808276a9777b16c177822032892cb2a0
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-07-13 15:20:50 +0500

    delete old model in course serializers and views

--- api/v1/course/serializers.py
+++ api/v1/course/serializers.py
@@ -16,16 +16,3 @@ class SendHomeworkSerializer(serializers.ModelSerializer):
     class Meta:
         model = HomeworkSubmission
         fields = ('homework', 'student', 'upload_homework_time', 'submission_homework_file','submission_rating',)
-
-
-class CreateHomeworkStudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HomeworkStudent
-        fields = ('student', 'teacher', 'homework_title', 'homework_text', 'homework_file',
-            'homework_created_time', 'homework_deadline_time',)
-
-
-class AnswerHomeworkStudentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HomeworkAnswer
-        fields = ('homework_task', 'student', 'upload_homework_time', 'answer_file', 'answer_rating',)
--- api/v1/course/views.py
+++ api/v1/course/views.py
@@ -9,8 +9,6 @@ from rest_framework.generics import CreateAPIView
 from courses.models import (
     Homework, 
     HomeworkSubmission,
-    HomeworkAnswer,
-    HomeworkStudent
     )
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -28,14 +26,5 @@ class SendHomework(CreateAPIView):
     queryset = HomeworkSubmission.objects.all()
     serializer_class = SendHomeworkSerializer
 
-
-class HomeworkStudents(CreateAPIView):
-    queryset = HomeworkStudent.objects.all()
-    serializer_class = CreateHomeworkStudentSerializer
-
     def perform_create(self, serializer):
-        serializer.save(teacher = self.request.user.teacher)
-
-class AnswerStudents(CreateAPIView):
-    queryset = HomeworkAnswer.objects.all()
-    serializer_class = AnswerHomeworkStudentSerializer
+        serializer.save(student = self.request.user.student)
