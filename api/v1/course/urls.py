81ad8d6a75dc68d28399963f18fb96249b2aa234
Begzod Rasulev <begzod_rasulev@mail.ru>
2022-07-13 15:26:13 +0500

    delete course url

--- api/v1/course/urls.py
+++ api/v1/course/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .views import CreateHomework, SendHomework, HomeworkStudents, AnswerStudents
+from .views import CreateHomework, SendHomework
 
 urlpatterns = [
     path('homework/create/', CreateHomework.as_view()),
     path('homework/answer/', SendHomework.as_view()),
-    path('homework/student/create/', HomeworkStudents.as_view()),
-    path('homework/student/answer/', AnswerStudents.as_view())
 ]
